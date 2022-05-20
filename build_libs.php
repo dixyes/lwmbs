a1e7ef92661641949461dfa17b08c7513c48f7e0
dixyes <dixyes@gmail.com>
2022-05-20 15:57:22 +0800

    Add lib only builder

---
+++ build_libs.php
@@ -0,0 +1,81 @@
+#!php
+<?php
+/**
+ * Copyright (c) 2022 Yun Dou <dixyes@gmail.com>
+ *
+ * lwmbs is licensed under Mulan PSL v2. You can use this
+ * software according to the terms and conditions of the
+ * Mulan PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *
+ * http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS,
+ * WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+spl_autoload_register(function ($class) {
+    if (strpos($class, '\\') !== false) {
+        // never here
+        throw new Exception('???');
+    }
+
+    $osDir = match (PHP_OS_FAMILY) {
+        'Windows', 'WINNT', 'Cygwin' => 'windows',
+        'Linux' => 'linux',
+        'Darwin' => 'macos',
+    };
+
+    if (str_starts_with($class, 'Lib') && $class !== 'Library') {
+        $libName = substr($class, 3);
+        $file = __DIR__ . "/$osDir/libraries/$libName.php";
+        require $file;
+        return;
+    }
+
+    $file = __DIR__ . "/$osDir/$class.php";
+    if (is_file($file)) {
+        require $file;
+    } else {
+        require __DIR__ . "/common/$class.php";
+    }
+});
+
+
+function mian($argv): int
+{
+    Util::setErrorHandler();
+
+    $config = new Config($argv);
+
+    $libNames = [
+        'libssh2',
+        'curl',
+        'zlib',
+        'brotli',
+        'libiconv',
+        'libffi',
+        'openssl',
+        'libzip',
+        'bzip2',
+        'nghttp2',
+        'onig',
+        'xz',
+    ];
+    foreach ($libNames as $name) {
+        $lib = new ("Lib$name")($config);
+        $config->addLib($lib);
+    }
+    //var_dump(array_map(fn($x)=>$x->getName(),$config->makeLibArray()));
+
+    foreach ($config->makeLibArray() as $lib) {
+        $lib->prove();
+    }
+
+    return 0;
+}
+
+exit(mian($argv));
