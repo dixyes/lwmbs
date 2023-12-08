32b218428814f426cf6fa2e11d958e010bba2f71
dixyes <dixyes@gmail.com>
2023-12-08 15:47:22 +0800

    Add 8.3 things

--- fetch_source.php
+++ fetch_source.php
@@ -45,7 +45,7 @@ function mian($argv): int
             'versionFile' => ['PATH', false, null, 'output versions to a file'],
             'shallowClone' => ['BOOL', false, false, 'use shallow clone'],
             'srcFile' => ['SRCFILE', false, __DIR__ . DIRECTORY_SEPARATOR . 'src.json', 'src.json path'],
-            'phpVer' => ['VERSION', false, '8.2', 'php version in major.minor format like 8.2'],
+            'phpVer' => ['VERSION', false, '8.3', 'php version in major.minor format like 8.3'],
         ],
     );
 
